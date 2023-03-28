805ec9304cc34f28afaf2b8ed5138d9af56ca2a8
Payson Parker <payson.parker@comcast.net>
2023-03-28 12:19:20 -0700

    Added logging

--- src/main/java/com/gcu/business/NoteRestService.java
+++ src/main/java/com/gcu/business/NoteRestService.java
@@ -1,11 +1,12 @@
 package com.gcu.business;
 
 import com.gcu.model.NoteModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -17,6 +18,8 @@ import java.util.Map;
 @RequestMapping("/note-service")
 public class NoteRestService {
 
+    Logger logger = LoggerFactory.getLogger(NoteRestService.class);
+
     @Autowired
     NoteBusinessService noteBusinessService;
 
@@ -27,6 +30,8 @@ public class NoteRestService {
     @GetMapping(path="/notes", produces= {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<?> getNotesAsJson()
     {
+        logger.info("==========> ENTER NoteRestService.getNotesAsJson() at " + "/notes");
+
         try
         {
             // Get notes
@@ -35,17 +40,24 @@ public class NoteRestService {
             // If note doesn't exist
             if(notes == null)
             {
+                logger.info("==========> EXIT NoteRestService.getNotesAsJson() at " + "/notes");
+
                 // Return not found
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
             else
             {
+                logger.info("==========> EXIT NoteRestService.getNotesAsJson() at " + "/notes");
+
                 // Return note and OK
                 return new ResponseEntity<>(notes, HttpStatus.OK);
             }
         }
         catch(Exception e)
         {
+            logger.error("==========> EXCEPTION NoteRestService.getNotesAsJson() at " + "/notes", e);
+            logger.info("==========> EXIT NoteRestService.getNotesAsJson() at " + "/notes");
+
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -53,6 +65,8 @@ public class NoteRestService {
     @GetMapping(path="/note-{noteIdNumber}", produces= {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<?> getNoteById(@PathVariable("noteIdNumber") int noteIdNumber)
     {
+        logger.info("==========> ENTER NoteRestService.getNoteById() at " + "/note-{}", noteIdNumber);
+
         try
         {
             // Get user
@@ -61,23 +75,32 @@ public class NoteRestService {
             // If user doesn't exist
             if(note == null)
             {
+                logger.info("==========> EXIT NoteRestService.getNoteById() at " + "/note-{}", noteIdNumber);
+
                 // Return not found
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
             else
             {
+                logger.info("==========> EXIT NoteRestService.getNoteById() at " + "/note-{}", noteIdNumber);
+
                 // Return user and OK
                 return new ResponseEntity<>(note, HttpStatus.OK);
             }
         }
         catch(Exception e)
         {
+            logger.error("==========> EXCEPTION NoteRestService.getNoteById() at " + "/note", e);
+            logger.info("==========> EXIT NoteRestService.getNoteById() at " + "/note-{}", noteIdNumber);
+
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
     @GetMapping(path="/notes/{userIdNumber}", produces= {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<?> getNotesByUserId(@PathVariable("userIdNumber") int userIdNumber)
     {
+        logger.info("==========> ENTER NoteRestService.getNotesByUserId() at " + "/notes/{}", userIdNumber);
+
         try
         {
             // Get user
@@ -86,17 +109,24 @@ public class NoteRestService {
             // If user doesn't exist
             if(notes == null)
             {
+                logger.info("==========> EXIT NoteRestService.getNotesByUserId() at " + "/notes/{}", userIdNumber);
+
                 // Return not found
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
             else
             {
+                logger.info("==========> EXIT NoteRestService.getNotesByUserId() at " + "/notes/{}", userIdNumber);
+
                 // Return user and OK
                 return new ResponseEntity<>(notes, HttpStatus.OK);
             }
         }
         catch(Exception e)
         {
+            logger.error("==========> EXCEPTION NoteRestService.getNotesByUserId() at " + "/notes/", e);
+            logger.info("==========> EXIT NoteRestService.getNotesByUserId() at " + "/notes/{}", userIdNumber);
+
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -109,7 +139,11 @@ public class NoteRestService {
     @PostMapping("/notes")
     public void addNote(@RequestBody NoteModel note)
     {
+        logger.info("==========> ENTER NoteRestService.addNote() at " + "/notes");
+
         noteBusinessService.addNote(note);
+
+        logger.info("==========> EXIT NoteRestService.addNote() at " + "/notes");
     }
 
     /**
@@ -118,10 +152,15 @@ public class NoteRestService {
      */
     @PutMapping(path="/notes/{noteIdNumber}")
     public ResponseEntity<NoteModel> updateNote(@PathVariable(value = "noteIdNumber") int noteIdNumber, @RequestBody NoteModel noteDetails) {
+        logger.info("==========> ENTER NoteRestService.updateNote() at " + "/notes/{}", noteIdNumber);
+
         NoteModel note = noteBusinessService.getNoteById(noteIdNumber);
         note.setTitle(noteDetails.getTitle());
         note.setText(noteDetails.getText());
         noteBusinessService.update(note);
+
+        logger.info("==========> EXIT NoteRestService.updateNote() at " + "/notes/{}", noteIdNumber);
+
         return ResponseEntity.ok(note);
     }
 
@@ -131,10 +170,15 @@ public class NoteRestService {
      */
     @DeleteMapping(path="/notes/{noteIdNumber}")
     public Map<String, Boolean> deleteNote(@PathVariable(value = "noteIdNumber") int noteIdNumber) throws Exception {
+        logger.info("==========> ENTER NoteRestService.deleteNote() at " + "/notes/{}", noteIdNumber);
+
         NoteModel note = noteBusinessService.getNoteById(noteIdNumber);
         noteBusinessService.delete(note);
         Map<String, Boolean> response = new HashMap<>();
         response.put("deleted", Boolean.TRUE);
+        
+        logger.info("==========> EXIT NoteRestService.deleteNote() at " + "/notes/{}", noteIdNumber);
+
         return response;
 }
 }
--- src/main/java/com/gcu/business/UserRestService.java
+++ src/main/java/com/gcu/business/UserRestService.java
@@ -1,6 +1,8 @@
 package com.gcu.business;
 
 import com.gcu.model.UserModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -14,6 +16,8 @@ import java.util.List;
 @RequestMapping("/user-service")
 public class UserRestService {
 
+    Logger logger = LoggerFactory.getLogger(UserRestService.class);
+
     @Autowired
     UserBusinessService userBusinessService;
 
@@ -24,6 +28,8 @@ public class UserRestService {
     @GetMapping(path="/users", produces= {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<?> getUsersAsJson()
     {
+        logger.info("==========> ENTER UserRestService.getUsersAsJson() at " + "/users");
+
         try
         {
             // Get users
@@ -32,17 +38,24 @@ public class UserRestService {
             // If user doesn't exist
             if(users == null)
             {
+                logger.info("==========> EXIT UserRestService.getUsersAsJson() at " + "/users");
+
                 // Return not found
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
             else
             {
+                logger.info("==========> EXIT UserRestService.getUsersAsJson() at " + "/users");
+
                 // Return user and OK
                 return new ResponseEntity<>(users, HttpStatus.OK);
             }
         }
         catch(Exception e)
         {
+            logger.error("==========> EXCEPTION UserRestService.getUsersAsJson() at " + "/users", e);
+            logger.info("==========> EXIT UserRestService.getUsersAsJson() at " + "/users");
+
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -50,6 +63,8 @@ public class UserRestService {
     @GetMapping(path="/user-{userIdNumber}", produces= {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<?> getUserById(@PathVariable("userIdNumber") int useridNumber)
     {
+        logger.info("==========> ENTER UserRestService.getUserById() at " + "/users-{}", useridNumber);
+
         try
         {
             // Get user
@@ -58,17 +73,24 @@ public class UserRestService {
             // If user doesn't exist
             if(user == null)
             {
+                logger.info("==========> EXIT UserRestService.getUserById() at " + "/users-{}", useridNumber);
+
                 // Return not found
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
             else
             {
+                logger.info("==========> EXIT UserRestService.getUserById() at " + "/users-{}", useridNumber);
+
                 // Return user and OK
                 return new ResponseEntity<>(user, HttpStatus.OK);
             }
         }
         catch(Exception e)
         {
+            logger.error("==========> EXCEPTION UserRestService.getUserById()", e);
+            logger.info("==========> EXIT UserRestService.getUserById() at " + "/users-{}", useridNumber);
+
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
